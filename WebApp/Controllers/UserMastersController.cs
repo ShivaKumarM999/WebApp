e7270c6cc9e49ad90499be31b3d517e223fbbd58
SHIVA <SHIVA@DESKTOP-KMPH0BP>
2021-07-14 18:04:17 +0530

    Add project files.

---
+++ WebApp/Controllers/UserMastersController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class UserMastersController : Controller
+    {
+        private POCEntities db = new POCEntities();
+
+        public ActionResult Index()
+        {
+            return View(db.UserMasters.ToList());
+        }
+
+        public ActionResult SignUp()
+        {
+
+            ViewBag.CountryList = new SelectList(GetCountryList(), "CountryId", "CountryName");
+            return View();
+        }
+
+        public List<Country> GetCountryList()
+        {
+            List<Country> countries = db.Countries.ToList();
+            return countries;
+        }
+
+        public ActionResult GetStateList(int Cid)
+        {
+            try
+            {
+                List<State> selectList = db.States.Where(x => x.CountryId == Cid).ToList();
+                ViewBag.Slist = new SelectList(selectList, "SId", "StateName");
+            }
+            catch (Exception e)
+            {
+                string exception = e.Message;
+            }
+            return PartialView("DisplayStates");
+        }
+
+        public string Decrypt(string encrString)
+        {
+            byte[] b;
+            string decrypted;
+            try
+            {
+                b = Convert.FromBase64String(encrString);
+                decrypted = System.Text.ASCIIEncoding.ASCII.GetString(b);
+
+            }
+            catch (FormatException fe)
+            {
+                decrypted = fe.Message;
+            }
+            return decrypted;
+        }
+        public string Enrypt(string strEncrypted)
+        {
+            byte[] b = System.Text.ASCIIEncoding.ASCII.GetBytes(strEncrypted);
+            string encrypted = Convert.ToBase64String(b);
+            return encrypted;
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(UserMaster userMaster)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    int UserExists = db.UserMasters.Where(u => u.UserName == userMaster.UserName).Count();
+                    if (UserExists > 0)
+                    {
+                        ViewBag.ErrorMessage = "User already exists, Please enter new User Name";
+                        ViewBag.CountryList = new SelectList(GetCountryList(), "CountryId", "CountryName");
+                        return View(userMaster);
+                    }
+                    userMaster.Password = Enrypt(userMaster.Password);
+                    db.UserMasters.Add(userMaster);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception e)
+            {
+                string exception = e.Message;
+            }
+            ViewBag.CountryList = new SelectList(GetCountryList(), "CountryId", "CountryName");
+            return View(userMaster);
+        }
+
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignIn(UserMaster userMaster)
+        {
+            
+            var validUser = db.UserMasters.Where(u => u.UserName == userMaster.UserName).FirstOrDefault();
+            string pwd = Decrypt(validUser.Password);
+            if (pwd==userMaster.Password)
+            { 
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Please enter valid User Name and Password";
+                return View(userMaster);
+            }
+        }
+        public ActionResult Details(UserMaster userMaster)
+        {
+            if (userMaster.UserName == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserMaster userMaster1 = db.UserMasters.Where(u=>u.UserName==userMaster.UserName).FirstOrDefault();
+            userMaster1.Password = Decrypt(userMaster1.Password);
+            if (userMaster == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userMaster1);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApp/Models/UserMaster1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    [MetadataType(typeof(UserMasterMetadata))]
+    public partial class UserMaster
+    {
+    }
+
+    public partial class UserMasterMetadata
+    {
+        public int UserId { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [RegularExpression("^[0-9a-zA-Z]+", ErrorMessage ="Please Enter Alphabets and Digits only")]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        [Range(18,100,ErrorMessage ="Age should be minimum 18")]
+        public int Age { get; set; }
+        [Required]
+        [Display(Name ="Country")]
+        public int CountryId { get; set; }
+        [Required]
+        [Display(Name = "State")]
+        public int StateId { get; set; }
+    }
+}
